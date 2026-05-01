773635bfd34008388a143bc4fcc3c83f91cfd936
Aniket <rajputaniket121@gmail.com>
2026-05-01 16:29:19 +0530

    added chnages in usecase

--- ORSProject-10/src/main/java/com/rays/ctl/SchedulerJobCtl.java
+++ ORSProject-10/src/main/java/com/rays/ctl/SchedulerJobCtl.java
@@ -1,8 +1,13 @@
 package com.rays.ctl;
 
+import java.util.List;
+
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import com.rays.common.BaseCtl;
+import com.rays.common.DropdownList;
+import com.rays.common.ORSResponse;
 import com.rays.dto.SchedulerJobDTO;
 import com.rays.form.SchedulerJobForm;
 import com.rays.service.SchedulerJobServiceInt;
@@ -10,6 +15,15 @@ import com.rays.service.SchedulerJobServiceInt;
 @RestController
 @RequestMapping(value = "SchedulerJob")	
 public class SchedulerJobCtl extends BaseCtl<SchedulerJobDTO, SchedulerJobForm, SchedulerJobServiceInt> {
+	
+	@GetMapping(value = "/preload")
+	public ORSResponse preload() {
+		ORSResponse res = new ORSResponse(true);
+		List<DropdownList> statusList =	service.search(null, userContext);
+		res.addResult("statusList", statusList);
+		
+		return res;
+	}
 
 
 }
